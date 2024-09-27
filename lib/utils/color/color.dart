ce2b79c5a8af0edf53df7cdb036fad6b3535aa90
sanghyuk <ish775800@gmail.com>
2024-09-27 20:47:26 +0900

    feat: color 변수 개선

--- lib/utils/color/color.dart
+++ lib/utils/color/color.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0XFFCDC1FF);
-const kBackgroundColor = Color(0XFFA594F9);
-const titleColor = Color(0XFF5E17ED);
+// const kPrimaryColor = Color(0XFFCDC1FF);
+const kPrimaryColor = Color.fromRGBO(205, 193, 255, 1);
+// const kBackgroundColor = Color(0XFFA594F9);
+const kBackgroundColor = Color.fromRGBO(165, 148, 249, 1);
+// const titleColor = Color(0XFF5E17ED);
+const titleColor = Color.fromRGBO(94, 23, 237, 1);
 
-const oneColor = Color(0XFFF5EFFF);
-const twoColor = Color(0XFFE5D9F2);
-const threeColor = Color(0XFFCDC1FF);
-const fourColor = Color(0XFFA594F9);
-
+// const oneColor = Color(0XFFF5EFFF);
+const oneColor = Color.fromRGBO(245, 239, 255, 1);
+// const twoColor = Color(0XFFE5D9F2);
+const twoColor = Color.fromRGBO(229, 217, 242, 1);
+// const threeColor = Color(0XFFCDC1FF);
+const threeColor = Color.fromRGBO(205, 193, 255, 1);
+// const fourColor = Color(0XFFA594F9);
+const fourColor = Color.fromRGBO(165, 148, 249, 1);
