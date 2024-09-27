0e369f1188ad2721626cec7b58a076ca7cda5352
Inwoong <dlsdnd122@naver.com>
2024-09-27 19:40:51 +0900

    feat: title 수정

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'NTAOIog',
+      title: 'NTAOIo',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
           primaryColor: kPrimaryColor,
