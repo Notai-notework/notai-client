bdab3f099d6bb8df82902e18e427a937a3184128
sanghyuk <ish775800@gmail.com>
2024-09-27 20:47:06 +0900

    feat: Scaffold 커스텀 위젯 개선

--- lib/widgets/global/custom_scaffold.dart
+++ lib/widgets/global/custom_scaffold.dart
@@ -1,22 +1,42 @@
 import 'package:flutter/material.dart';
 
-class CustomScaffold extends Scaffold {
-  CustomScaffold({
+class CustomScaffold extends StatefulWidget {
+  final Widget appBarTitle;
+  final Widget body;
+
+  const CustomScaffold({
     Key? key,
-    required Widget body, // Scaffold body
-    required Widget appBarTitle, // Scaffold appBarTitle (위젯)
-  }) : super(
-            key: key,
-            appBar: AppBar(
-              title: appBarTitle,
-            ),
-            body: body,
-            bottomNavigationBar: Container(
-                child: BottomNavigationBar(
-              items: const [
-                BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'Chat'),
-                BottomNavigationBarItem(icon: Icon(Icons.chat), label: 'What')
-              ],
-              selectedItemColor: Colors.red,
-            )));
+    required this.appBarTitle,
+    required this.body,
+  }) : super(key: key);
+
+  @override
+  State<CustomScaffold> createState() => _CustomScaffoldState();
+}
+
+class _CustomScaffoldState extends State<CustomScaffold> {
+  int _currentIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: widget.appBarTitle),
+        body: widget.body,
+        bottomNavigationBar: Container(
+            child: BottomNavigationBar(
+          currentIndex: _currentIndex,
+          onTap: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          items: const [
+            BottomNavigationBarItem(
+                icon: Icon(Icons.file_copy_rounded), label: '문서'),
+            BottomNavigationBarItem(icon: Icon(Icons.chat), label: '커뮤니티'),
+            BottomNavigationBarItem(icon: Icon(Icons.bookmark), label: '즐겨찾기'),
+          ],
+          selectedItemColor: Colors.red,
+        )));
+  }
 }
